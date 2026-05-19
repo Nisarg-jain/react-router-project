754c8ed6ca3d9bc72f01cdf8b17df5c91444a2b6
Nisarg-jain <jainnisag98@gmail.com>
2026-05-19 19:49:49 +0530

    Initial commit: React Router project with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='w-full h-screen bg-gradient-to-r from-blue-500 to-black-600 flex items-center justify-center'>
+        <h1 className='text-5xl font-bold text-white'>Hello, Tailwind!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
